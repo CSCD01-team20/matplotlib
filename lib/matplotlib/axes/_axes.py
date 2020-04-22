91f9426dd90946ffbb73b32fa272957d77544d1a
Robert <robert.augustynowicz@mail.utoronto.ca>
2020-04-22 15:47:42 -0400

    made appropriate changes to the bachelors degree example, as well as removed comments to make code cleaner

--- lib/matplotlib/axes/_axes.py
+++ lib/matplotlib/axes/_axes.py
@@ -826,47 +826,35 @@ class Axes(_AxesBase):
             last_y = handle.get_ydata()[-1]
             return last_x, last_y
 
-        # Get last data point for each handle
         xys = np.array([get_last_data_point(x) for x in handles])
 
-        # Find the largest last x and y value
         data_maxx, data_maxy = np.max(xys, axis=0)
-        # Find the smallest last y value
         data_miny = np.min(xys, axis=0)[1]
 
-        # Get the figure width and height in pixels
         fig_dpi_transform = self.figure.dpi_scale_trans.inverted()
         fig_bbox = self.get_window_extent().transformed(fig_dpi_transform)
         fig_width_px = fig_bbox.width * self.figure.dpi
         fig_height_px = fig_bbox.height * self.figure.dpi
 
-        # Get the figure width and height in points
         fig_minx, fig_maxx = self.get_xbound()
         fig_miny, fig_maxy = self.get_ybound()
         fig_width_pt = abs(fig_maxx - fig_minx)
         fig_height_pt = abs(fig_maxy - fig_miny)
 
-        # Space to the left of the text converted from pixels
         margin_left = 8 * (fig_width_pt / fig_width_px)
-        # Space to the top and bottom of the text converted from pixels
         margin_vertical = 2 * (fig_height_pt / fig_height_px)
 
         text_fontsize = 10
-        # Height of the text converted from pixels
         text_height = text_fontsize * (fig_height_pt / fig_height_px)
 
-        # Height of each bucket
         bucket_height = text_height + 2 * margin_vertical
-        # Total number of buckets
         buckets_total = 1 + int((fig_maxy - fig_miny - text_height) /
                                 bucket_height)
-        # Bit array to track which buckets are used
         buckets_map = 0
 
         def get_bucket_index(y):
             return int((y - fig_miny) / bucket_height)
 
-        # How many text SHOULD be in this bucket
         bucket_densities = [0] * buckets_total
         for xy in xys:
             data_x, data_y = xy
@@ -886,7 +874,6 @@ class Axes(_AxesBase):
 
         bucket_offset = None
         prev_ideal_bucket = -1
-        # Iterate over all the handles, labels, and xy data sorted by y asc
         for handle, label, xy in sorted(filter(in_viewport,
                                                zip(handles, labels, xys)),
                                         key=by_y):
@@ -894,16 +881,12 @@ class Axes(_AxesBase):
 
             ideal_bucket = get_bucket_index(data_y)
             if ideal_bucket != prev_ideal_bucket and ideal_bucket < len(bucket_densities):
-                # If a bucket is dense, then we want to center the text around
-                # the bucket. SSo, find out how many empty buckets there are
-                # below the current bucket
                 bucket_density = bucket_densities[ideal_bucket]
                 ideal_offset = bucket_density // 2
                 empty_buckets_below = 0
                 for i in range(ideal_bucket + 1,
                                ideal_bucket - ideal_offset + 1,
                                -1):
-                    # If reached bottom or bucket is not empty
                     if i == 0 or buckets_map & (1 << i) == 1:
                         break
                     empty_buckets_below += 1
@@ -913,20 +896,15 @@ class Axes(_AxesBase):
             bucket = ideal_bucket + bucket_offset
             text_x, text_y = None, None
 
-            # Find the nearest empty bucket
             for index in range(buckets_total):
-                # 0 <= bucket_index <= buckets_total
                 bucket_index = max(0, min(bucket + index, buckets_total))
                 bucket_mask = 1 << bucket_index
-                # If this bucket is empty
                 if buckets_map & bucket_mask == 0:
-                    # Mark this bucket as used
                     buckets_map |= bucket_mask
                     text_x = data_maxx + margin_left
                     text_y = bucket_index * bucket_height + fig_miny
                     break
 
-            # If a bucket was found for the text
             if text_x is not None and text_y is not None:
                 text_color = handle.get_color()
                 line_label = self.annotate(label, (data_maxx, data_y),
@@ -936,20 +914,6 @@ class Axes(_AxesBase):
                                            color=text_color,
                                            annotation_clip=True)
                 self._line_labels.append(line_label)
-
-        # A bucket is not necessarily aligned with the data point
-        # If space allows, center the text around the data point
-        # TODO(omarchehab98): group fuzzing
-        # for line_label in self._line_labels:
-        #     datax, datay = line_label.xy
-        #     targety = datay - text_height / 2
-        #     textx, texty = line_label.get_position()
-        #     direction = 1 if targety > texty else -1
-        #     target_bucket = get_bucket_index(targety) + direction
-        #     if (get_bucket_index(targety) == get_bucket_index(texty) and
-        #             (target_bucket == -1 or
-        #              buckets_map & (1 << target_bucket) == 0)):
-        #         line_label.set_position((textx, targety))
 
     def has_label_lines(self):
         return self._line_labels is not None
